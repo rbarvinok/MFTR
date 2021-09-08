f4d4e8c185985fe744f2b59c951ed2cb558c1c0f
barvinok <rbarvinok@ukr.net>
2021-09-08 20:16:52 +0300

    08092021
    Signed-off-by: barvinok <rbarvinok@ukr.net>

--- src/main/java/ua/SL520/javaclass/servisClass/SaveToWord.java
+++ src/main/java/ua/SL520/javaclass/servisClass/SaveToWord.java
@@ -9,11 +9,13 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
+import ua.SL520.javaclass.domain.Result;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static ua.SL520.controller.Controller.*;
 
@@ -27,7 +29,7 @@ public class SaveToWord {
         headerContent = "ДЕРЖАВНИЙ НАУКОВО-ДОСЛІДНИЙ ІНСТИТУТ ВИПРОБУВАНЬ І СЕРТИФІКАЦІЇ ОЗБРОЄННЯ ТА ВІЙСЬКОВОЇ ТЕХНІКИ ";
         footerContent = " 14033 м. Чернігів ";
 
-        fileContent = "Дані вимірювань " + openFile;
+        fileContent = "Дані вимірювань";
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Зберегти як...");
@@ -96,6 +98,8 @@ public class SaveToWord {
         document.setTextPosition(25);
         document.setText(fileContent);
         document.addBreak();
+        document.setText(openFile);
+        //document.addBreak();
 
         XWPFParagraph bodyParagraph1 = docxModel.createParagraph();
         XWPFRun document1 = bodyParagraph1.createRun();
@@ -104,50 +108,81 @@ public class SaveToWord {
         document1.setFontFamily("Time New Roman");
         document1.addBreak();
 
+//-------------------  table  -------------
         XWPFTable table = docxModel.createTable();
         int cellCount = StringUtils.countMatches(headFile, ",");
         int rownum = 0;
-
+        String splitBy = ",";
+        String line;
+//------------------   1  ----------------------------
         XWPFTableRow tableRow = table.getRow(0);
-        tableRow.getCell(0);
-//        tableRow.addNewTableCell().setText("Номер");
-//        tableRow.addNewTableCell().setText("Номер1111");
 
+        line = Arrays.toString(headFile.split(splitBy));
+        line = line.replace("[", "").replace("]", "");
+
+
+        //---
         for (int colnum = 0; colnum <= cellCount; colnum++) {
-
-
-            tableRow.addNewTableCell().setText(headFile.split(",").toString());
-            //cell.setCellValue(headFile.split(",")[colnum]);
-
+            tableRow.addNewTableCell();
+            tableRow.getCell(colnum).setText(line);
         }
-//
-//
-//        rownum++;
-//        XWPFTableRow tableRow1 = table.getRow(rownum);
-//        tableRow1.getCell(0).setText("Назва файлу");
-//        tableRow1.addNewTableCell().setText("Номер");
-//        rownum++;
+        //---
+        for (Result result : resultStream) {
+            tableRow = table.createRow();
 
-//        headFile = "Назва файлу, Номер, Дата, Час, Швидкість, Похибка, Точки, FitOrder, Примітка";
+            for (int colnum = 0; colnum <= cellCount; colnum++) {
+                line = resultStream.get(colnum).toString();
+                line = line.replace("[", "").replace("]", "");
+                String[] fields = line.split(splitBy, -1);
+                //tableRow.getCell(colnum).setText(Arrays.toString(result.toString().split(splitBy)));
+                tableRow.getCell(colnum).setText(fields.toString());
+            }
+        }
 
+        //-------------------------------    2    -------------------------------------------------
+//        List<String[]> csvData = new ArrayList<String[]>();
+//        csvData.addAll(Collections.singleton(resultStream.toString().split(",")));
+//        System.out.println(csvData);
+//        int cols = cellCount;
+//        int rows = (int)Math.ceil(resultStream.size());
+//        table = docxModel.createTable(rows, cols);
+//        XWPFTableRow row;
+//        XWPFTableCell cell;
+//        int i = 0;
+//
+//        for (int r = 0; r < rows; r++) {
+//
+//            row = table.getRow(r);
+//            for (int c = 0; c <= cols; c++) {
+//                cell = row.getCell(c);
+//                if (i < resultStream.size()) cell.setText(csvData.get(i)[1]);
+//                i++;
+//            }
+//        }
+
+//---------------------------------     0    ----------------------------------------------------
+//        XWPFTable table = docxModel.createTable();
+//        int cellCount = StringUtils.countMatches(headFile, ",");
+//        int rownum = 0;
+//
+//        XWPFTableRow tableRow = table.getRow(0);
+//        String line = Arrays.toString(headFile.split(","));
+//        System.out.println(line);
 //
 //        for (int colnum = 0; colnum <= cellCount; colnum++) {
-//            cell = row.createCell(colnum, CellType.STRING);
-//            cell.setCellValue(headFile.split(",")[colnum]);
-//            sheet.autoSizeColumn(colnum);
+//            tableRow.getCell(0);
+//            tableRow.addNewTableCell().setText(line);
 //        }
-//        rownum++;
+////        headFile = "Назва файлу, Номер, Дата, Час, Швидкість, Похибка, Точки, FitOrder, Примітка";
 //
-//        for (
-//                Result result : resultStream) {
-//            row = sheet.createRow(rownum);
-//            for (int colnum = 0; colnum <= columnCount; colnum++) {
-//                cell = row.createCell(colnum, CellType.STRING);
-//                cell.setCellValue(result.toString().split(",")[colnum]);
-//                //sheet.autoSizeColumn(colnum);
+//        for (Result result : resultStream) {
+//            tableRow = table.createRow();
+//
+//            for (int colnum = 0; colnum <= cellCount; colnum++) {
+//                tableRow.addNewTableCell().setText(Arrays.toString(result.toString().split(",")));
 //            }
-
-
+//        }
+//-------------------------------------------------------------------------------------
         // сохраняем модель docx документа в файл
         FileOutputStream outputStream = null;
         try {
