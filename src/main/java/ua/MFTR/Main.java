a07f24d1ba2f314ed456fa81ddfb78bf5769f838
barvinok <rbarvinok@ukr.net>
2022-09-06 09:33:53 +0300

    06092022

---
+++ src/main/java/ua/MFTR/Main.java
@@ -0,0 +1,38 @@
+package ua.MFTR;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import ua.MFTR.javaclass.servisClass.AlertAndInform;
+
+public class Main extends Application {
+    AlertAndInform inform = new AlertAndInform();
+    public static String icoImage = "/images/img.png";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/shotSample.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResource(icoImage).toExternalForm()));
+        primaryStage.setTitle("SL-520PE Reporter");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+
+        primaryStage.setOnCloseRequest(event ->
+        {
+            inform.title = "Вихід з програми";
+            inform.hd = "Закрити програму?";
+            inform.ct = "Всі незбережені дані буде втрачено";
+            inform.confirmation(event);
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
